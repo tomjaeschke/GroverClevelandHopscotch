183cf3dec14c6d8252d7c129d2a7febb95d7ce1a
Tom Jaeschke <tomjaeschke@tomjaeschke.com>
2019-03-27 21:19:49 -0500

    I am making the means for digging up an IP address less fragile.
    There is now a second source to pull from should the first fail.

--- src/GroverClevelandHopscotch.Infrastructure/Dependencies/IpMechanics.cs
+++ src/GroverClevelandHopscotch.Infrastructure/Dependencies/IpMechanics.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 using System.Text;
 using GroverClevelandHopscotch.Core.Interfaces;
@@ -8,14 +9,52 @@ namespace GroverClevelandHopscotch.Infrastructure.Dependencies
     {
         public string GetIpAddress()
         {
-            int counter = 0;
-            string url = "https://www.expressvpn.com/what-is-my-ip";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            string failover = null;
+            string[] scrappings = new string[] { };
             StringBuilder scrappingSpool = new StringBuilder();
+            try
+            {
+                Scrape("https://www.expressvpn.com/what-is-my-ip", ref scrappingSpool);
+            }
+            catch (Exception exception)
+            {
+                scrappingSpool = new StringBuilder();
+                Scrape("https://www.ultratools.com/tools/yourIPResult", ref scrappingSpool);
+                scrappings = scrappingSpool.ToString().Split("\"".ToCharArray());
+                foreach (string scrapping in scrappings)
+                {
+                    if (scrapping.Contains("/tools/ipWhoisLookupResult?ipAddress"))
+                    {
+                        failover = scrapping.Split("=".ToCharArray())[1].Replace("\n", "");
+                    }
+                }
+            }
+            if (failover != null) return failover;
+            scrappings = scrappingSpool.ToString().Split(">".ToCharArray());
+            int counter = 0;
+            foreach (string scrapping in scrappings)
+            {
+                if (scrapping.IndexOf("Your public IP address") > -1)
+                {
+                    return scrappings[counter + 2].Split("<".ToCharArray())[0].Replace("\n", "");
+                }
+                if (scrapping.IndexOf("Your IP address") > -1)
+                {
+                    return scrappings[counter + 2].Split("<".ToCharArray())[0].Replace("\n", "");
+                }
+                counter++;
+            }
+            return null;
+        }
+
+        private void Scrape(string url, ref StringBuilder scrappingSpool)
+        {
+            int counter = 0;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
             {
                 using (Stream stream = response.GetResponseStream())
-                {                  
+                {
                     byte[] buffer = new byte[1000000];
                     do
                     {
@@ -28,21 +67,6 @@ namespace GroverClevelandHopscotch.Infrastructure.Dependencies
                     } while (counter > 0);
                 }
             }
-            string[] scrappings = scrappingSpool.ToString().Split(">".ToCharArray());
-            counter = 0;
-            foreach (string scrapping in scrappings)
-            {
-                if (scrapping.IndexOf("Your public IP address") > -1)
-                {
-                    return scrappings[counter + 2].Split("<".ToCharArray())[0];
-                }
-                if (scrapping.IndexOf("Your IP address") > -1)
-                {
-                    return scrappings[counter + 2].Split("<".ToCharArray())[0];
-                }
-                counter++;
-            }
-            return null;
         }
     }
 }
