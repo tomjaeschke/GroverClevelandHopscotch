c25073501c452de4ed4240c925285a2903a6c814
Tom Jaeschke <tomjaeschke@tomjaeschke.com>
2019-05-06 18:26:51 -0500

    I am starting to set up a scenario in which a smart component feeds dumb components in the draggable example.
    I am handing into one component as an input a list of presidents.

--- src/GroverClevelandHopscotch.UserInterface/src/app/components/storeExample.component.ts
+++ src/GroverClevelandHopscotch.UserInterface/src/app/components/storeExample.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { President } from '../models/president.model';
+import { PresidentialContract } from '../contracts/presidential.contract';
 @Component({
     selector: 'store',
     templateUrl: './storeExample.component.html',
     styleUrls:  ['./storeExample.component.css']
 })
-export class StoreExampleComponent {
+export class StoreExampleComponent implements OnInit {
+    presidents:Array<President>;
     
+    ngOnInit(): void{
+        this.presidentialContract.getPresidents().toPromise().then(
+            function(data) {
+                this.presidents = data;
+            }.bind(this),
+            function(error){
+                console.log(error);
+            });
+    }
+
+    constructor(public presidentialContract : PresidentialContract) { }
 }
--- src/GroverClevelandHopscotch.UserInterface/src/app/components/storeExampleEdit.component.ts
+++ src/GroverClevelandHopscotch.UserInterface/src/app/components/storeExampleEdit.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { President } from '../models/president.model';
 @Component({
     selector: 'store-edit',
     templateUrl: './storeExampleEdit.component.html',
     styleUrls:  ['./storeExampleEdit.component.css']
 })
 export class StoreExampleComponentEdit {
+    @Input('presidents') presidents:Array<President>;
+
     public startDrag(event: any){
         console.log('start');
         console.log(event);
